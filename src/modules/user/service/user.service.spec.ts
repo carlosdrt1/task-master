ffdf824783f0117eca934c724fd858c6c009b913
carlosdrt1 <carlosduarteinfo@gmail.com>
2025-07-20 13:26:56 -0300

    test(user): add test to findByEmail and existsByEmail methods

--- src/modules/user/service/user.service.spec.ts
+++ src/modules/user/service/user.service.spec.ts
@@ -5,6 +5,7 @@ import { UserRepository } from '../repository/user.repository';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserCreateFixture } from '../testing/fixture/user-create.fixture';
 import { UserResponseFixture } from '../testing/fixture/user-response.fixture';
+import { UserInternalFixture } from '../testing/fixture/user-internal.fixture';
 
 describe('UserService', () => {
   let userService: UserService;
@@ -20,6 +21,10 @@ describe('UserService', () => {
     userService = module.get<UserService>(UserService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('validate definition', () => {
     expect(userService).toBeDefined();
   });
@@ -31,6 +36,44 @@ describe('UserService', () => {
 
       expect(result).toEqual(UserResponseFixture);
       expect(MockUserRepository.create).toHaveBeenCalledWith(data);
+    });
+  });
+
+  describe('get user by email', () => {
+    it('should return a user', async () => {
+      const email: string = 'user@email.com';
+      const result = await userService.getByEmail(email);
+
+      expect(result).toEqual(UserInternalFixture);
+      expect(MockUserRepository.findByEmail).toHaveBeenCalledWith(email);
+    });
+
+    it('return null if user was not found', async () => {
+      const email: string = 'notfound@gmail.com';
+      MockUserRepository.findByEmail.mockResolvedValue(null);
+      const result = await userService.getByEmail(email);
+
+      expect(result).toBe(null);
+      expect(MockUserRepository.findByEmail).toHaveBeenCalledWith(email);
+    });
+  });
+
+  describe('verify if user exists by email', () => {
+    it('should return true if user exists', async () => {
+      const email: string = 'user@email.com';
+      const result = await userService.existsByEmail(email);
+
+      expect(result).toBe(true);
+      expect(MockUserRepository.existsByEmail).toHaveBeenCalledWith(email);
+    });
+
+    it('should return false if user was not found', async () => {
+      const email: string = 'notfound@gmail.com';
+      MockUserRepository.existsByEmail.mockResolvedValue(false);
+      const result = await userService.existsByEmail(email);
+
+      expect(result).toBe(false);
+      expect(MockUserRepository.existsByEmail).toHaveBeenCalledWith(email);
     });
   });
 });
--- src/modules/user/testing/mock/user.repository.mock.ts
+++ src/modules/user/testing/mock/user.repository.mock.ts
@@ -1,5 +1,8 @@
+import { UserInternalFixture } from '../fixture/user-internal.fixture';
 import { UserResponseFixture } from '../fixture/user-response.fixture';
 
 export const MockUserRepository = {
   create: jest.fn().mockResolvedValue(UserResponseFixture),
+  findByEmail: jest.fn().mockResolvedValue(UserInternalFixture),
+  existsByEmail: jest.fn().mockResolvedValue(true),
 };
