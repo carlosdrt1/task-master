dfe91a35be1520f5ef0de25da6f16c259620c2ac
carlosdrt1 <carlosduarteinfo@gmail.com>
2025-07-19 22:55:30 -0300

    feat(auth): implement login route

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,23 @@ import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
 import { UserModule } from '../user/user.module';
 import { SharedModule } from '@/shared/shared.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, SharedModule],
+  imports: [
+    UserModule,
+    SharedModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '7d',
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { CreateUserDto } from '@/modules/user/dto/create-user.dto';
+import { LoginDto } from '../dto/login.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +11,12 @@ export class AuthController {
   @Post('register')
   async register(@Body() body: CreateUserDto) {
     return this.authService.register(body);
+  }
+
+  @Post('login')
+  async login(@Res() res: Response, @Body() body: LoginDto) {
+    const token = await this.authService.login(body);
+    res.cookie('token', token);
+    return res.status(200).json({ message: 'login successful' });
   }
 }
