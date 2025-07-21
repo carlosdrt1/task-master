3ebae3cd5911cc7cfa21bf6bddf1b68fa95f1fbf
carlosdrt1 <carlosduarteinfo@gmail.com>
2025-07-21 19:49:58 -0300

    test(user): unit tests for get, delete and update methods in UserService

--- src/modules/user/service/user.service.spec.ts
+++ src/modules/user/service/user.service.spec.ts
@@ -6,6 +6,7 @@ import { CreateUserDto } from '../dto/create-user.dto';
 import { UserCreateFixture } from '../testing/fixture/user-create.fixture';
 import { UserResponseFixture } from '../testing/fixture/user-response.fixture';
 import { UserInternalFixture } from '../testing/fixture/user-internal.fixture';
+import { ConflictException } from '@nestjs/common';
 
 describe('UserService', () => {
   let userService: UserService;
@@ -74,6 +75,64 @@ describe('UserService', () => {
 
       expect(result).toBe(false);
       expect(MockUserRepository.existsByEmail).toHaveBeenCalledWith(email);
+    });
+  });
+
+  describe('get user by id', () => {
+    it('should return a user', async () => {
+      const id = '4cbde839-2ee6-4cc2-bf75-e056b938d74e';
+      const result = await userService.getById(id);
+
+      expect(result).toEqual(UserResponseFixture);
+      expect(MockUserRepository.findById).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe('check email conflict', () => {
+    it('should return nothing if email is not registered', async () => {
+      const email = 'newEmail@gmail.com';
+      const id = '4cbde839-2ee6-4cc2-bf75-e056b938d74e';
+      jest
+        .spyOn(userService, 'getByEmail')
+        .mockResolvedValue(UserInternalFixture);
+      const result = await userService.checkEmailConflict(email, id);
+
+      expect(result).toBe(undefined);
+    });
+
+    it('should throw an ConflictException if email is already registered', async () => {
+      const email = 'email@gmail.com';
+      const id = 'different id';
+      jest
+        .spyOn(userService, 'getByEmail')
+        .mockResolvedValue(UserInternalFixture);
+      await expect(userService.checkEmailConflict(email, id)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+  });
+
+  describe('update user by id', () => {
+    it('should return a updated user', async () => {
+      const id = '4cbde839-2ee6-4cc2-bf75-e056b938d74e';
+      const data = { name: 'newName' };
+      jest
+        .spyOn(userService, 'checkEmailConflict')
+        .mockResolvedValue(undefined);
+      const result = await userService.update(id, data);
+
+      expect(result).toEqual(UserResponseFixture);
+      expect(MockUserRepository.update).toHaveBeenCalledWith(id, data);
+    });
+  });
+
+  describe('delete a user by id', () => {
+    it('should return nothing if ther user was deleted', async () => {
+      const id = '4cbde839-2ee6-4cc2-bf75-e056b938d74e';
+      const result = await userService.delete(id);
+
+      expect(result).toBe(undefined);
+      expect(MockUserRepository.delete).toHaveBeenCalledWith(id);
     });
   });
 });
