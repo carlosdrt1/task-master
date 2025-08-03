2b16b7a9b44e0bb33584e2b6a9caaa9c527c0003
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-08-03 17:02:52 -0300

    refactor: change implementation for create task and remove util for date

--- src/modules/task/service/task.service.ts
+++ src/modules/task/service/task.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { TaskRepository } from '../repository/task.repository';
 import { CreateTaskDto } from '../dto/create-task.dto';
-import { zeroTimeDate } from '@/shared/utils/date.util';
 import { TaskWithTodoList } from '../types/task-with-todo.type';
 import { TaskInput } from '../types/task-input.type';
 
@@ -12,8 +11,6 @@ export class TaskService {
   async create(data: CreateTaskDto, userId: string): Promise<TaskWithTodoList> {
     const inputData: TaskInput = {
       ...data,
-      start: data.start ? zeroTimeDate(data.start) : zeroTimeDate(new Date()),
-      end: zeroTimeDate(data.end),
       userId,
     };
 
--- src/shared/utils/date.util.ts
+++
@@ -1,5 +0,0 @@
-export function zeroTimeDate(date: Date): Date {
-  const zeroDate = new Date(date);
-  zeroDate.setHours(0, 0, 0, 0);
-  return zeroDate;
-}
